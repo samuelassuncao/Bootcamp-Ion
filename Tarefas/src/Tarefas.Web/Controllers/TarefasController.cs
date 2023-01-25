02fc6feb800a0a0d27a934ecb3be269877b24286
samuelassuncao <samuel.coutoassuncao@gmail.com>
2023-01-25 14:49:44 -0300

    Criado o botão detalhes, lista de tarefas mudada de lugar para ser acessível por todos os métodos

--- Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
@@ -6,6 +6,16 @@ namespace Tarefas.Web.Controllers;
 
 public class TarefasController : Controller
 {
+    public List<TarefaViewModel> listaDeTarefas { get; set; }
+    public TarefasController()
+        {
+            listaDeTarefas = new List<TarefaViewModel>()
+        {
+            new TarefaViewModel() { Id = 1, Titulo = "Escovar os Dentes", Descricao = "Todas as manhãs"},
+            new TarefaViewModel() { Id = 2, Titulo = "Arrumar a cama", Descricao = "Todas as manhãs"},
+            new TarefaViewModel() { Id = 3, Titulo = "Tirar o lixo", Descricao = "Segunda, quarta e sexta"}
+        };
+        }
     public IActionResult Create()
     {
         return View();
@@ -13,12 +23,13 @@ public class TarefasController : Controller
 
     public IActionResult Index()
     {
-        var listaDeTarefas = new List<TarefaViewModel>()
-        {
-            new TarefaViewModel() { Titulo = "Escovar os Dentes", Descricao = "Usar sensodyne"},
-            new TarefaViewModel() { Titulo = "Arrumar a cama"},
-            new TarefaViewModel() { Titulo = "Tirar o lixo", Descricao = "Segunda, quarta e sexta"}
-        };
+        
         return View(listaDeTarefas);
+    }
+
+    public IActionResult Details (int id)
+    {
+        var tarefa = listaDeTarefas.Find(tarefa=> tarefa.Id == id);
+        return View(tarefa);
     }
 }
