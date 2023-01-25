c7e9e1be2456a5241e8096ea27f192130afd60e3
samuelassuncao <samuel.coutoassuncao@gmail.com>
2023-01-25 13:57:04 -0300

    Criados os botões Criar Tarefa, Lista de Tarefas e feita a lista de tarefas em TarefasController e Index

--- Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
+++ Tarefas/src/Tarefas.Web/Controllers/TarefasController.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.AspNetCore.Mvc;
+using Tarefas.Web.Models;
 
 namespace Tarefas.Web.Controllers;
 
@@ -8,5 +9,16 @@ public class TarefasController : Controller
     public IActionResult Create()
     {
         return View();
+    }
+
+    public IActionResult Index()
+    {
+        var listaDeTarefas = new List<TarefaViewModel>()
+        {
+            new TarefaViewModel() { Titulo = "Escovar os Dentes", Descricao = "Usar sensodyne"},
+            new TarefaViewModel() { Titulo = "Arrumar a cama"},
+            new TarefaViewModel() { Titulo = "Tirar o lixo", Descricao = "Segunda, quarta e sexta"}
+        };
+        return View(listaDeTarefas);
     }
 }
